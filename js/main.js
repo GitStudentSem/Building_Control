8824a42b6df0d7b0cc24ce7473743025cf634236
GitStudentSem <programmSem@yandex.ru>
2021-01-14 16:23:21 +0300

    create header & adaptive

--- js/main.js
+++ js/main.js
@@ -8,4 +8,18 @@ $(function () {
   $(".close").click(function () {
     $(".modal").hide();
   });
+  // Бургер меню
+  var burger = document.querySelector(".burger");
+  burger.addEventListener("click", function () {
+    document
+      .querySelector(".header__list")
+      .classList.toggle("header__list-visible");
+  });
+  // Бургер кнопка
+  (function () {
+    const burgerBtn = document.querySelector(".burger");
+    burgerBtn.addEventListener("click", () => {
+      burgerBtn.classList.toggle("burger__active");
+    });
+  })();
 });
