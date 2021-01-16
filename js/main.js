6bad200e2feea670c04d645695b2bbc91fb45e01
GitStudentSem <programmSem@yandex.ru>
2021-01-16 14:15:01 +0300

    fix media width content

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 $(function () {
-  // Скрипт модаьного окна
-  // Для использования откопировать этот кусок, а так же
-  // div class="modal" и файл _modal.sass
-  // $(".modal__open").click(function () {
-  //   $(".modal").show();
-  // });
-  // $(".close").click(function () {
-  //   $(".modal").hide();
-  // });
   // Бургер меню
   var burger = document.querySelector(".burger");
   burger.addEventListener("click", function () {
